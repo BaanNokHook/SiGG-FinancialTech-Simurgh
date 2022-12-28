f6c6c27fd99a0e3594306faf8ddd93d107f8c862
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 14:46:16 +0700

    SiGG-FinancialTech-Simurgh-GMCommonLibs-Quater-ConvertHelper.cs

---
+++ ConvertHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
+
+namespace GM.CommonLibs.Helper
+{
+    public static class ConvertHelper
+    {
+        public static List<T> DataTableToList<T>(this DataTable table) where T : class, new()
+        {
+            try
+            {
+                List<T> list = new List<T>();
+
+                foreach (var row in table.AsEnumerable())
+                {
+                    T obj = new T();
+
+                    foreach (var prop in obj.GetType().GetProperties())
+                    {
+                        try
+                        {
+                            PropertyInfo propertyInfo = obj.GetType().GetProperty(prop.Name);
+                            propertyInfo.SetValue(obj, Convert.ChangeType(row[prop.Name], propertyInfo.PropertyType), null);
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+
+                    list.Add(obj);
+                }
+
+                return list;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static List<T> DataTableToListForNull<T>(this DataTable dt) where T : class, new()
+        {
+            List<T> lstItems = new List<T>();
+            if (dt != null && dt.Rows.Count > 0)
+                foreach (DataRow row in dt.Rows)
+                    lstItems.Add(ConvertDataRowToGenericType<T>(row));
+            else
+                lstItems = null;
+            return lstItems;
+        }
+
+        private static T ConvertDataRowToGenericType<T>(DataRow row) where T : class, new()
+        {
+            Type entityType = typeof(T);
+            T objEntity = new T();
+            foreach (DataColumn column in row.Table.Columns)
+            {
+                object value = row[column.ColumnName];
+                if (value == DBNull.Value) value = null;
+                PropertyInfo property = entityType.GetProperty(column.ColumnName, BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
+                try
+                {
+                    if (property != null && property.CanWrite)
+                        property.SetValue(objEntity, value, null);
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return objEntity;
+        }
+
+        public static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            DataTable dataTable = new DataTable();
+            PropertyDescriptorCollection propertyDescriptorCollection =
+                TypeDescriptor.GetProperties(typeof(T));
+            for (int i = 0; i < propertyDescriptorCollection.Count; i++)
+            {
+                PropertyDescriptor propertyDescriptor = propertyDescriptorCollection[i];
+                Type type = propertyDescriptor.PropertyType;
+
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    type = Nullable.GetUnderlyingType(type);
+
+                dataTable.Columns.Add(propertyDescriptor.Name, type);
+            }
+            object[] values = new object[propertyDescriptorCollection.Count];
+            foreach (T iListItem in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = propertyDescriptorCollection[i].GetValue(iListItem);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
