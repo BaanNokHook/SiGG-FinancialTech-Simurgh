3e59b43181263bf1e3e8f1e04b8fd4c6384a12a9
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-28 14:51:30 +0700

    SiGG-FinancialTech-Simurgh-GMCommonLibs-Quater-Globals

---
+++ Globals.cs
@@ -0,0 +1,23 @@
+﻿namespace GM.CommonLibs.Helper
+{
+    public static class Globals
+    {
+        public static int GetStatusOrder(string status)
+        {
+            if (status != null)
+            {
+                switch (status.ToLower())
+                {
+                    case "delete": return 1;
+                    case "create": return 2;
+                    case "update": return 3;
+                    default: return 99;
+                }
+            }
+            else
+            {
+                return 99;
+            }
+        }
+    }
+}
